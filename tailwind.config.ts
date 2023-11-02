74b96bdd6aa3858cdec9c98e2f46ff633b914fae
Thomas <tomspace.800@gmail.com>
2023-11-02 20:43:54 +0100

    Initialisation tailwind/css/icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	mode: 'jit',
+	purge: {
+		content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+	},
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				lightgrey: '#ffffff80',
+				turquoise: '#68B8B5D9',
+				gray: '#758B9E80',
+				blue: '#0083cc',
+				purple: '#7000c9',
+				yellow: '#f4dc00',
+				bordeaux: '#98296b',
+			},
+			fontFamily: {
+				main: ['Sono', 'monospace'],
+				cursive: ['Kaushan Script', 'cursive'],
+			},
+			spacing: {},
+		},
+	},
+	plugins: [],
+};
+export default config;
